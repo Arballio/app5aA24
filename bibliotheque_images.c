e07ca916e534c985ca7b410ceb5e2a8910b62411
Arballio <crakblob@gmail.com>
2024-11-04 09:16:07 -0500

    Ajout de la fonction d'histogramme
    Ajout des fichiers de base avec un petit formatage

---
+++ bibliotheque_images.c
@@ -0,0 +1,130 @@
+/********
+Fichier: bibliotheque_images.c
+Auteurs: Domingo Palao Munoz
+		 Charles-Antoine Brunet
+Date: 28 octobre 2023
+Description: Fichier de distribution pour GEN145.
+********/
+
+#include "bibliotheque_images.h"
+
+
+
+int pgm_lire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+			 int *p_lignes, int *p_colonnes,
+			 int *p_maxval, struct MetaData *p_metadonnees)
+{	int status = ERREUR;
+
+
+	FILE *fp = fopen(nom_fichier, "r");
+
+	if (fp == NULL) {
+	   return -1;
+	}
+
+
+	return status;
+}
+
+int pgm_ecrire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR],
+			   int lignes, int colonnes,
+			   int maxval, struct MetaData metadonnees)
+{
+	int status = ERREUR;
+
+
+	return status;
+}
+
+int pgm_copier(int matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, int matrice2[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes2, int *p_colonnes2)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_creer_histogramme(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int histogramme[MAX_VALEUR+1])
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_couleur_preponderante(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_eclaircir_noircir(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval, int valeur)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_creer_negatif(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_extraire(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, int lignes2, int colonnes2, int *p_lignes, int *p_colonnes)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_sont_identiques(int matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, int matrice2[MAX_HAUTEUR][MAX_LARGEUR], int lignes2, int colonnes2)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int pgm_pivoter90(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int sens)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+// Operations pour les images couleurs
+int ppm_lire(char nom_fichier[], struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int *p_maxval, struct MetaData *p_metadonnees)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int ppm_ecrire(char nom_fichier[], struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int lignes, int colonnes, int maxval, struct MetaData metadonnees)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int ppm_copier(struct RGB matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, struct RGB matrice2[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes2, int *p_colonnes2)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int ppm_sont_identiques(struct RGB matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, struct RGB matrice2[MAX_HAUTEUR][MAX_LARGEUR], int lignes2, int colonnes2)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
+int ppm_pivoter90(struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int sens)
+{
+	int status = ERREUR;
+
+	return status;
+}
+
